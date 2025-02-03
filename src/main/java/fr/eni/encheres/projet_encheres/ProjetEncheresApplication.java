39e72d302c4642a535418ab5d7b2e32069668e23
Pierrick Kerlau <pierrick.kerlau@gmail.com>
2025-02-04 00:33:16 +0100

    Build - Changement du package, du nom du projet gradle et de la version de gradle

--- src/main/java/fr/eni/encheres/projet_encheres/ProjetEncheresApplication.java
+++ src/main/java/fr/eni/encheres/projet_encheres/ProjetEncheresApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package fr.eni.encheres.projet_encheres;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EncheresApplication {
+public class ProjetEncheresApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EncheresApplication.class, args);
+		SpringApplication.run(ProjetEncheresApplication.class, args);
 	}
 
 }
---
+++ src/test/java/fr/eni/encheres/projet_encheres/ProjetEncheresApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.eni.encheres.projet_encheres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetEncheresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
