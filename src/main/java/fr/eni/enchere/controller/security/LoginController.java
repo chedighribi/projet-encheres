694cb5177532bef81a53e5e1f5b1c3e61fd5958f
chedighribi <chady.ghribi.gc@gmail.com>
2025-02-04 09:25:25 +0100

    feat: login page and spring security

---
+++ src/main/java/fr/eni/enchere/controller/security/LoginController.java
@@ -0,0 +1,13 @@
+package fr.eni.enchere.controller.security;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+	@GetMapping("/login")
+	String login() {
+		return "view-login";
+	}
+	
+}
