515d5a78b97007d07f1b32727729072592b561f4
ValentinG <valentin.godec2024@campus-eni.fr>
2025-02-06 10:14:10 +0100

    fix: ajout de methodes

--- src/main/java/fr/eni/encheres/bll/ArticleAVendreServiceImpl.java
+++ src/main/java/fr/eni/encheres/bll/ArticleAVendreServiceImpl.java
@@ -4,20 +4,52 @@ import java.util.List;
 
 import org.springframework.stereotype.Service;
 
+import fr.eni.encheres.bo.Adresse;
 import fr.eni.encheres.bo.ArticleAVendre;
+import fr.eni.encheres.bo.Categorie;
 import fr.eni.encheres.dal.ArticleAVendreDAO;
+import fr.eni.encheres.dal.CategorieDAO;
 
 @Service
 public class ArticleAVendreServiceImpl implements ArticleAVendreService{
+	private CategorieDAO categorieDao;
 	private ArticleAVendreDAO ArticleAVendreDao;
 	
-	public ArticleAVendreServiceImpl(ArticleAVendreDAO ArticleAVendreDao) {
+	public ArticleAVendreServiceImpl(ArticleAVendreDAO ArticleAVendreDao, CategorieDAO categorieDao) {
 		this.ArticleAVendreDao=ArticleAVendreDao;
+		this.categorieDao=categorieDao;
 	}
 	
 	@Override
 	public List<ArticleAVendre> consulterArticles() {
 		return ArticleAVendreDao.findAll();
 	}
+
+	@Override
+	public void creerArticle(ArticleAVendre articleAVendre) {
+		ArticleAVendreDao.insertArticle(articleAVendre);
+	}
+
+	@Override
+	public List<Categorie> consulterCategories() {
+		return categorieDao.findAll();
+	}
+
+	@Override
+	public Categorie consulterCategorieParId(long id) {
+		return categorieDao.findById(id);
+	}
+
+	@Override
+	public List<Adresse> consulterAdresse() {
+		return null;
+	}
+
+	@Override
+	public Adresse consulterAdresseParId(long id) {
+		return null;
+	}
+
+	
 
 }
