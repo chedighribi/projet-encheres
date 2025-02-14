14bce25667713ef6bff66e5ca66ae917e2af3e6c
Pierrick Kerlau <pierrick.kerlau@gmail.com>
2025-02-14 12:30:54 +0100

    feat : error page

--- src/main/java/fr/eni/encheres/dal/EnchereDAOImpl.java
+++ src/main/java/fr/eni/encheres/dal/EnchereDAOImpl.java
@@ -21,10 +21,9 @@ import fr.eni.encheres.bo.Utilisateur;
 public class EnchereDAOImpl implements EnchereDAO {
 
 	private final String INSERT = "INSERT INTO ENCHERES (date_enchere, montant_enchere, id_utilisateur, no_article) VALUES(:date_enchere, :montant_enchere, :id_utilisateur, :no_article)";
+	private final String DELETE = "DELETE encheres WHERE id_utilisateur = :id_utilisateur";
 	private final String FIND_BY_ARTICLE= "SELECT date_enchere, montant_enchere, id_utilisateur, no_article FROM ENCHERES WHERE no_article = :id";
 	private final String FIND_BY_USER = "SELECT date_enchere, montant_enchere, id_utilisateur, no_article FROM ENCHERES WHERE id_utilisateur = :pseudo";
-	private final String FIND_HIGHEST = "SELECT TOP 1 * FROM encheres WHERE no_article = :articleId ORDER BY montant_enchere DESC";
-
 	
 	@Autowired
 	private NamedParameterJdbcTemplate jdbcTemplate;
@@ -53,13 +52,6 @@ public class EnchereDAOImpl implements EnchereDAO {
 	    namedParameter.addValue("pseudo", pseudo);
 	    return jdbcTemplate.query(FIND_BY_USER, namedParameter, new EnchereRowMapper());
 	}
-	
-	@Override
-	public Enchere readHighestEnchere(long articleId) {
-	    MapSqlParameterSource namedParameter = new MapSqlParameterSource();
-	    namedParameter.addValue("articleId", articleId);
-        return jdbcTemplate.queryForObject(FIND_HIGHEST, namedParameter, new EnchereRowMapper());
-	}
 
 	class EnchereRowMapper implements RowMapper<Enchere> {
 
@@ -80,10 +72,15 @@ public class EnchereDAOImpl implements EnchereDAO {
 	}
 
 	@Override
+	public Enchere readHighestEnchere(long articleId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
 	public void deleteEnchere(String pseudo) {
 		// TODO Auto-generated method stub
 		
 	}
-
 
 }
